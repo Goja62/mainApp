d424560028aa9e117571e0e18dd8b8339f47eef1
Goja <gortnar@beograd.com>
2026-03-03 18:31:31 +0100

    Blog Posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+    public  function showCreatePost()
+    {
+        return view('create-post');
+    }
+
+    public function storeNewPost(Request $request)
+    {
+        // Validation
+        $incomingFields = $request->validate([
+            'title' => ['required', 'max:255'],
+            'body' => 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $incomingFields['user_id'] = Auth::id();
+
+        // Store the post
+        $newPost = Post::create($incomingFields);
+        session()->flash('success', 'Your post was created successfully.');
+        return redirect('/create-post');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-
+// User Related Routes
 Route::get('/', [UserController::class, 'showCorrectHomepage']);
-
 Route::post('/register', [UserController::class, 'registerUser']);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout']);
+
+// Post Related Routes
+Route::get('/create-post', [PostController::class, 'showCreatePost']);
+Route::post('/create-post', [PostController::class, 'storeNewPost']);
