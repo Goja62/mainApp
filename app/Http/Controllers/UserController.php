451df062510ba13ffb8c848012676ec2df104b32
Goja <gortnar@beograd.com>
2026-03-03 11:32:25 +0100

    Validation & Details

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -12,14 +13,21 @@ class UserController extends Controller
     {
         $incomingFields = $request->validate(
             [
-                'username' => 'required|max:255',
-                'email' => 'required|email|max:255',
-                'password' => 'required|min:5|max:255'
+                'username' => ['required', 'min:3', 'max:255', Rule::unique('users', 'username')],
+                'email' => ['required', 'email', 'max:255', Rule::unique('users', 'email')],
+                'password' => ['required', 'min:5', 'max:255', 'confirmed']
             ],
             [
                 'username.required' => 'Molim vas da unesete korisničko ime.',
+                'username.min' => 'Korisničko ime mora imati najmanje 3 karaktera.',
+                "username.unique" => 'Korisničko ime već postoji, molim vas da odaberete drugo.',
                 'email.required' => 'Molim vas da unesete email adresu.',
-                'password.required' => 'Molim vas da unesete lozinku.'
+                'email.email' => 'Molim vas da unesete ispravnu email adresu.',
+                'email.unique' => 'Email adresa već postoji, molim vas da odaberete drugu.',
+                'email.max' => 'Email adresa ne smije biti duža od 255 karaktera.',
+                'password.required' => 'Molim vas da unesete lozinku.',
+                'password.min' => 'Lozinka mora imati najmanje 5 karaktera.',
+                'password.confirmed' => 'Lozinka i potvrda lozinke se ne poklapaju.'
             ]
         );
         User::create($incomingFields);
