2af52998f80dd4f9b9e8297021cc309be5b57a16
Goja <gortnar@beograd.com>
2026-03-23 08:08:49 +0100

    Scheduling Tasks

---
+++ resources/views/recapemail.blade.php
@@ -0,0 +1,5 @@
+<div style="font-family: sans-serif">
+    <p>Her is recap</p>
+    <p>Post Count: <strong>{{ $postConut }}</strong></p>
+    <p>User Count: <strong>{{ $userCount }}</strong></p>
+</div>
