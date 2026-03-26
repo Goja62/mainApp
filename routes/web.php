340935d8b40876d2981efad3b4a36f3a36da1972
Goja <gortnar@beograd.com>
2026-03-26 11:08:01 +0100

    Profile SPA (Part 1)

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,10 @@ Route::get('/profile/{user:username}', [UserController::class, 'profile']);
 Route::get('/profile/{user:username}/followers', [UserController::class, 'profileFollowers']);
 Route::get('/profile/{user:username}/following', [UserController::class, 'profileFollowing']);
 
+Route::get('/profile/{user:username}/raw', [UserController::class, 'profileRaw']);
+Route::get('/profile/{user:username}/followers/raw', [UserController::class, 'profileFollowersRaw']);
+Route::get('/profile/{user:username}/following/raw', [UserController::class, 'profileFollowingRaw']);
+
 
 Route::get('/admins-only', function () {
     return 'You cannot view this page';
